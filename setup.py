997ef8fd3ea1603700d162521162e699a8f80c6a
Stephen Macke <stephen.macke@gmail.com>
2024-07-06 20:20:37 -0700

    add python 3.12 in setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     keywords='speechrecognition asr voiceactivitydetection vad webrtc',
     ext_modules=[module],
